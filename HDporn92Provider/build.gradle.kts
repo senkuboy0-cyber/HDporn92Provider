70777bacab8bf59b5a76122a18131c32bda5d5e7
senkuboy0-cyber <senkuboy0@gmail.com>
2026-04-15 14:59:19 +0600

    Remove cloudstream block from module build.gradle.kts - it should be in root build.gradle.kts only

--- HDporn92Provider/build.gradle.kts
+++ HDporn92Provider/build.gradle.kts
@@ -1,12 +1,3 @@
-cloudstream {
-    setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/senkuboy0-cyber/HDporn92Provider")
-    version = 1
-    description = "HDporn92 - Free Premium Porn Videos"
-    authors = listOf("senkuboy0-cyber")
-    language = "en"
-    tvTypes = listOf("Movie", "TvSeries", "Others")
-}
-
 android {
     namespace = "com.hdporn92"
 }
